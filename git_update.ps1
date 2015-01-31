b24197d6dad428bb654390d8ff456e46b90a1af4
mkennedy04 <kennedym@hotmail.com>
2015-01-30 17:31:49 -0800

    adding the first version of my automated deployment script!!!

---
+++ git_update.ps1
@@ -0,0 +1,8 @@
+﻿$destination = "C:\users\mkennedy\onedrive\Projects\repos\simplehtml"
+if (!$commit_msg) {
+    $commit_msg = "just updating simpleindex.html"
+    }
+cd $destination
+git add index.html
+git commit -m $commit_msg
+git push
