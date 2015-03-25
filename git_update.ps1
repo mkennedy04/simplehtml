da4d553805a02740c368875f0218ccbc8d776ba6
mkennedy04 <kennedym@hotmail.com>
2015-03-25 14:02:08 -0700

    [03/25/2015 14:02:07] just updating simplehtml

--- git_update.ps1
+++ git_update.ps1
@@ -1,8 +1,28 @@
-﻿$destination = "C:\users\mkennedy\onedrive\Projects\repos\simplehtml"
-if (!$commit_msg) {
-    $commit_msg = "just updating simpleindex.html"
-    }
+﻿Param(
+    [string]$commit_msg
+)
+#goto the director of the local repo
+$destination = "C:\users\mkennedy\onedrive\Projects\repos\simplehtml"
 cd $destination
+
+#if no commit msg, use a generic one
+if (!$commit_msg) {
+    $commit_msg = "just updating simplehtml"
+    }
+
+# build a timestamp
+$date = Get-Date
+$timemsg = "[" + $date.ToLocalTime() + "]"
+$message = $timemsg + " " + $commit_msg
+
+# display the status of the current repo
+git-status
+
+# add files
 git add index.html
-git commit -m $commit_msg
+git add git_update.ps1
+# commit changes
+git commit -m $message
+# push to github
 git push
+# here you will be asked for your username/password
